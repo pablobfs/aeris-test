f946f7c25b9efd781128d64045799a85b6c68f84
Ignacio Rodrigo <irodrigo@codigodelsur.com>
2014-05-19 16:30:21 -0300

    Initial Commit

---
+++ AerisTestTests/AerisTestTests.m
@@ -0,0 +1,34 @@
+//
+//  AerisTestTests.m
+//  AerisTestTests
+//
+//  Created by Ignacio Rodrigo on 5/19/14.
+//  Copyright (c) 2014 Ignacio Rodrigo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AerisTestTests : XCTestCase
+
+@end
+
+@implementation AerisTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
